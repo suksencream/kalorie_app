e748e52f98ccace983c8088621f7f2b3e036ba29
Thet Hnin Satt <thethninsatt17@gmail.com>
2025-03-16 00:00:05 +0700

    pull

--- react_fitness/src/pages/Login/login.jsx
+++ react_fitness/src/pages/Login/login.jsx
@@ -17,7 +17,7 @@ const LoginPage = () => {
 
     try {
       // Send login request to the backend
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("http://localhost:5000/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
